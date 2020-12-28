e6c9e15f503c8ea16dd656ea16e06b2e58103df4
Madalina Cirstea <56949829+madalina-cirstea@users.noreply.github.com>
2020-12-28 12:28:07 +0200

    created empty .net project with authentication

---
+++ GreenHouse Management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GreenHouse_Management.MvcApplication" Language="C#" %>
---
+++ GreenHouse Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenHouse_Management.Startup))]
+namespace GreenHouse_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
