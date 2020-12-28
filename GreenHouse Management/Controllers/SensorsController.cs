ea4b15015e47e5baa68d07894a042c69b8e7cc74
Madalina Cirstea <56949829+madalina-cirstea@users.noreply.github.com>
2020-12-28 13:13:04 +0200

    added basic Sensor model with empty controller and Index view

---
+++ GreenHouse Management/Controllers/SensorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenHouse_Management.Controllers
+{
+    public class SensorsController : Controller
+    {
+        // GET: Sensors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GreenHouse Management/Models/Sensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GreenHouse_Management.Models
+{
+    public class Sensor
+    {
+        public int SensorId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        public float Value { get; set; }
+
+        [Required]
+        public string OperatingState { get; set; }
+    }
+}
