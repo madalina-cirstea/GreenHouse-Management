4db753bba3e9ab8cd74c1f1ebac62999435a6d81
IONELA MADALINA CIRSTEA <ionela.cirstea@s.unibuc.ro>
2021-01-09 15:29:52 +0200

    changed class structure in ProductCustomersViewModel.cs

--- GreenHouse Management/Models/ViewModels/ProductCustomersViewModel.cs
+++ GreenHouse Management/Models/ViewModels/ProductCustomersViewModel.cs
@@ -11,9 +11,19 @@ namespace GreenHouse_Management.Models.ViewModels
     public class ProductCustomersViewModel
     {
         [Required]
-        public Product Product { get; set; }
-        
+        [MinLength(5, ErrorMessage = "Product name cannot be less than 5!"),
+         MaxLength(20, ErrorMessage = "Product name cannot be more than 20!")]
+        public string Name { get; set; }
+
         [Required]
+        [MinLength(10, ErrorMessage = "Product name cannot be less than 10!"),
+         MaxLength(50, ErrorMessage = "Product name cannot be more than 50!")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Customer")]
+        public string CustomerId { get; set; }
+
         public IEnumerable<SelectListItem> CustomersList { get; set; }
     }
 }
