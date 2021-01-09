dab20cd381c38d0aeac35f092955830251fb9b45
IONELA MADALINA CIRSTEA <ionela.cirstea@s.unibuc.ro>
2021-01-09 21:52:06 +0200

    Added Admin and Customer Roles

--- GreenHouse Management/Startup.cs
+++ GreenHouse Management/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using GreenHouse_Management.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(GreenHouse_Management.Startup))]
@@ -9,6 +12,50 @@ namespace GreenHouse_Management
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateAdminAndCustomerRoles();
+        }
+
+        private void CreateAdminAndCustomerRoles()
+        {
+            var ctx = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                // create default admin
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+
+                var adminCreated = userManager.Create(user, "Admin2021!");
+                if (adminCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+
+                // create default customer
+                var user = new ApplicationUser();
+                user.UserName = "customer@customer.com";
+                user.Email = "customer@customer.com";
+
+                var adminCreated = userManager.Create(user, "Customer2021!");
+                if (adminCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Customer");
+                }
+            }
         }
     }
 }
